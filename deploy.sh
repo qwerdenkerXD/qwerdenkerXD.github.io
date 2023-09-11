c23a1faa962f977d07ceaa2e471782a6fa9b1ca6
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-09-11 21:06:07 +0200

    extended deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
+if [ ! -d "build" ] ;then
+    git clone -q -b page https://github.com/qwerdenkerXD/qwerdenkerXD.github.io build
+fi
 cd react_app
 npm run build
 cd ..
 cp html/* build
+cp LICENSE build
